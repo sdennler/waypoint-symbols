218fc60d5dacca66f15c888c780a27d14884e9a3
sdennler <den@vollbio.de>
2012-10-23 11:51:08 +0800

    New Symbols: Have Navids (not accessible yet)

--- rename_sym.sh
+++ rename_sym.sh
@@ -2,7 +2,11 @@
 
 for f in *.png
  do
-  name=`echo $f | sed -e "s/__/, /g" -e "s/_-/ \(/g" -e "s/-/\)/g" -e "s/_/ /g"`
+  name=`echo $f | sed\
+   -e "s/__/, /g"\
+   -e "s/_--/ \(/g"\
+   -e "s/--/\)/g"\
+   -e "s/_/ /g"`
   mv "$f" "finished/$name"
  done
 
